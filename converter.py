36247a691d02e5e72c505250f1d0c0918190dfa9
yunusacikoz <yunusacikoz@gmail.com>
2010-11-23 20:51:15 +0000

    Converter, exceptionlar kontrol edilip daha güvenli hale getirildi.

--- converter.py
+++ converter.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 from xml.dom.minidom import Document, CDATASection
 from xml.dom import minidom
 from PIL import Image
@@ -7,127 +7,150 @@ def pts2xml(ptsFile, xmlFile=None):
 	if xmlFile == None:
 		xmlFile = ptsFile[:-4] + ".xml"
 	
-	f = open(ptsFile, 'r')
-	fileContent = f.read().split()
-	start =  fileContent.index("{") + 1
-	end = fileContent.index("}")
-	pts = []
-	while start < end:
-		pts.append((float(fileContent[start]),float(fileContent[start+1])))
-		start += 2
-	f.close()
-		
-	doc = Document()
-
-	annotation = doc.createElement("annotation")
-	doc.appendChild(annotation)
-
-	objects = doc.createElement("objects")
-	objects.setAttribute("type", "points")
-	objects.setAttribute("count", "%.0f" % len(pts))
-	annotation.appendChild(objects)
-
-	text = ""
-	for (x,y) in pts:
-		text += "%.0f %.0f\n" % (x,y)
-	cDataSection = doc.createCDATASection(text)
-	objects.appendChild(cDataSection)
-
-	objects = doc.createElement("objects")
-	objects.setAttribute("type", "rectangles")
-	objects.setAttribute("count", "int")
-	annotation.appendChild(objects)
+	try:
+		f = open(ptsFile, 'r')
+		fileContent = f.read().split()
 	
-	cDataSection = doc.createCDATASection("\n")
-	objects.appendChild(cDataSection)
+	except IOError:
+		print "File: %s not found.\n" % asfFile
+		
+	else:	
+		start =  fileContent.index("{") + 1
+		end = fileContent.index("}")
+		pts = []
+		while start < end:
+			pts.append((float(fileContent[start]),float(fileContent[start+1])))
+			start += 2
+		f.close()
+			
+		doc = Document()
 
-	xml = open(xmlFile, 'w')
-	xml.write(doc.toprettyxml(indent="  ", encoding="UTF-8"))
-	xml.close()
+		annotation = doc.createElement("annotation")
+		doc.appendChild(annotation)
+
+		objects = doc.createElement("objects")
+		objects.setAttribute("type", "points")
+		objects.setAttribute("count", "%.0f" % len(pts))
+		annotation.appendChild(objects)
+
+		text = ""
+		for (x,y) in pts:
+			text += "%.0f %.0f\n" % (x,y)
+		cDataSection = doc.createCDATASection(text)
+		objects.appendChild(cDataSection)
+
+		objects = doc.createElement("objects")
+		objects.setAttribute("type", "rectangles")
+		objects.setAttribute("count", "int")
+		annotation.appendChild(objects)
+		
+		cDataSection = doc.createCDATASection("\n")
+		objects.appendChild(cDataSection)
+
+		xml = open(xmlFile, 'w')
+		xml.write(doc.toprettyxml(indent="  ", encoding="UTF-8"))
+		xml.close()
 	
 def xml2pts(xmlFile, ptsFile=None):
 	if ptsFile == None:
 		ptsFile = xmlFile[:-4] + ".pts"
 	
-	xmldoc = minidom.parse(xmlFile)
-	objects = xmldoc.getElementsByTagName("objects")
-	
-	pts = []
-	rects = []
-	
-	for object in objects:
-		lines = object.childNodes[1].data.splitlines()
-		if object.attributes["type"].value == "points":
-			for line in lines:
-				pts.append(line.split(' '))
-		elif object.attributes["type"].value == "rectangles":
-			for line in lines:
-				rects.append(line.split(' '))
-	
-	text = "version: 1\nn_points: %.0f\n{\n" % len(pts)
-	for (x,y) in pts:
-		text += "    %s %s\n" % (x,y)
-	
-	text += "}"
+	try:
+		xmldoc = minidom.parse(xmlFile)
+		objects = xmldoc.getElementsByTagName("objects")
 		
-	f = open(ptsFile, 'w')
-	f.write(text)
-	f.close
+	except IOError:
+		print "File: %s not found.\n" % asfFile
+		
+	else:
+		pts = []
+		rects = []
+		
+		for object in objects:
+			lines = object.childNodes[1].data.splitlines()
+			if object.attributes["type"].value == "points":
+				for line in lines:
+					pts.append(line.split(' '))
+			elif object.attributes["type"].value == "rectangles":
+				for line in lines:
+					rects.append(line.split(' '))
+		
+		text = "version: 1\nn_points: %.0f\n{\n" % len(pts)
+		for (x,y) in pts:
+			text += "    %s %s\n" % (x,y)
+		
+		text += "}"
+			
+		f = open(ptsFile, 'w')
+		f.write(text)
+		f.close
 	
 def asf2xml(asfFile, xmlFile=None):
 	if xmlFile == None:
 		xmlFile = asfFile[:-4] + ".xml"
 	
-	f = open(asfFile, 'r')
-	fileContent = f.readlines()
-
-	i = 0
-	while i < len(fileContent):
-		if fileContent[i].startswith("#") or fileContent[i] == '\n':
-			fileContent.pop(i)
-		else:
-			i += 1
-			
-	count = int(fileContent.pop(0))
-	imageStr = fileContent.pop(-1).rstrip()
-	(sx,sy) = Image.open(imageStr).size
-	index = 0
-	
-	pts = []
-	while index < count:
-		line = fileContent[index].split()
-		pts.append((float(line[2])*sx,float(line[3])*sy))
-		index += 1
-
-	f.close()
+	try:
+		f = open(asfFile, 'r')
+		fileContent = f.readlines()
+	except IOError:
+		print "File: %s not found.\n" % asfFile
 		
-	doc = Document()
+	else:
 
-	annotation = doc.createElement("annotation")
-	doc.appendChild(annotation)
+		i = 0
+		while i < len(fileContent):
+			if fileContent[i].startswith("#") or fileContent[i] == '\n':
+				fileContent.pop(i)
+			else:
+				i += 1
+				
+		count = int(fileContent.pop(0))
+		imageStr = fileContent.pop(-1).rstrip()
+		try:
+			if os.name == "nt":
+				(sx,sy) = Image.open(asfFile[:asfFile.rfind('\\')+1] + imageStr).size
+			elif os.name == "posix":
+				(sx,sy) = Image.open(asfFile[:asfFile.rfind('/')+1] + imageStr).size
+		except IOError:
+			print "Corresponding image file of the given asf file not found.\n"
+		else:
+			index = 0
+			
+			pts = []
+			while index < count:
+				line = fileContent[index].split()
+				pts.append((float(line[2])*sx,float(line[3])*sy))
+				index += 1
 
-	objects = doc.createElement("objects")
-	objects.setAttribute("type", "points")
-	objects.setAttribute("count", "%.0f" % len(pts))
-	annotation.appendChild(objects)
+			f.close()
+				
+			doc = Document()
 
-	text = ""
-	for (x,y) in pts:
-		text += "%.0f %.0f\n" % (x,y)
-	cDataSection = doc.createCDATASection(text)
-	objects.appendChild(cDataSection)
+			annotation = doc.createElement("annotation")
+			doc.appendChild(annotation)
 
-	objects = doc.createElement("objects")
-	objects.setAttribute("type", "rectangles")
-	objects.setAttribute("count", "int")
-	annotation.appendChild(objects)
-	
-	cDataSection = doc.createCDATASection("\n")
-	objects.appendChild(cDataSection)
+			objects = doc.createElement("objects")
+			objects.setAttribute("type", "points")
+			objects.setAttribute("count", "%.0f" % len(pts))
+			annotation.appendChild(objects)
 
-	xml = open(xmlFile, 'w')
-	xml.write(doc.toprettyxml(indent="  ", encoding="UTF-8"))
-	xml.close()
+			text = ""
+			for (x,y) in pts:
+				text += "%.0f %.0f\n" % (x,y)
+			cDataSection = doc.createCDATASection(text)
+			objects.appendChild(cDataSection)
+
+			objects = doc.createElement("objects")
+			objects.setAttribute("type", "rectangles")
+			objects.setAttribute("count", "int")
+			annotation.appendChild(objects)
+			
+			cDataSection = doc.createCDATASection("\n")
+			objects.appendChild(cDataSection)
+
+			xml = open(xmlFile, 'w')
+			xml.write(doc.toprettyxml(indent="  ", encoding="UTF-8"))
+			xml.close()
 
 if __name__ == "__main__":
     if len(sys.argv) == 4:
